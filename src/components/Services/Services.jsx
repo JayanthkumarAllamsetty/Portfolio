8cad225455e57c622e5546e9ecfce607b2e9c70d
@Jayanthkumar_001 <2000030043@kluniversity.in>
2023-04-16 22:16:58 +0530

    first commit

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,93 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './2000030043_Jayanth Kumar.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <spane>
+          I have good knowledge of DevOps, Developer and Cloud skills.
+          <br />
+          <br />
+          As a student I have done some projects on these platforms. I have good knowledge of MERN stack and JFSD. 
+          <br />
+          <br/>
+          And I have also completed few certifications on AWS , Juniper Networks and Redhat Enterprise Application Developer. 
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"DevOps"}
+            detail={" I have a good knowledge of Docker,Jenkins, Ansible , Git, Github etc. and I am learning it."}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Developer"}
+            detail={"Html, Css, JavaScript, React, Nodejs, Express. I have a basic knowledge on the MERN stack and I am learning it."}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"Cloud Skills"}
+            detail={
+              "I have a good knowledge of AWS and Azure cloud services. I have worked on AWS EC2, S3, Lambda, IAM etc. "
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
