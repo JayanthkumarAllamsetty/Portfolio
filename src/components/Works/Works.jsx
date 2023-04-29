fa97d8ba5128aca17c62a7966ee78731b5680e81
@Jayanthkumar_001 <2000030043@kluniversity.in>
2023-04-29 11:14:21 +0530

    modified:   src/components/Works/Works.jsx

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -44,10 +44,10 @@ const Works = () => {
       </div>
       <div className="w-right">
         <motion.div
-          initial={{ rotate: 45 }}
+          initial={{ rotate: 90 }}
           whileInView={{ rotate: 0 }}
           viewport={{ margin: "-40px" }}
-          transition={{ duration: 3.5, type: "spring" }}
+          transition={{ duration: 2, type: "spring" }}
           className="w-mainCircle"
         >
           <div className="w-secCircle">
