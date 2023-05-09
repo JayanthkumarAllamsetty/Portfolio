57c18751156f592d4e0fc7ce4c3d58cb864166b3
@Jayanthkumar_001 <2000030043@kluniversity.in>
2023-05-09 12:33:50 +0530

    updates

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react";
 import "./Portfolio.css";
 import { Swiper, SwiperSlide } from "swiper/react"
+import  { Autoplay } from "swiper";
 import "swiper/css";
+import "swiper/css/autoplay";
 import Sidebar from "../../img/Quiz Application.png";
 import Ecommerce from "../../img/Todo list.png";
 import HOC from "../../img/Healthcare.jpg";
@@ -10,6 +12,10 @@ import { themeContext } from "../../Context";
 const Portfolio = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
   return (
     <div className="portfolio" id="portfolio">
       {/* heading */}
@@ -17,11 +23,18 @@ const Portfolio = () => {
       <span>Portfolio</span>
 
       {/* slider */}
+   
       <Swiper
+      modules={[Autoplay]}
         spaceBetween={30}
         slidesPerView={3}
-        grabCursor={true}
         className="portfolio-slider"
+        loop={true}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+        }}
+        
       >
         <SwiperSlide>
           <img src={Sidebar} alt="" />
@@ -36,6 +49,7 @@ const Portfolio = () => {
           <img src={HOC} alt="" />
         </SwiperSlide>
       </Swiper>
+      
     </div>
   );
 };
